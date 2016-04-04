7b75f33eeea006e16a59d2667e55f41eb5ecc538
David Herrmann <dh.herrmann@gmail.com>
2016-04-04 12:34:46 +0200

    build: properly export symbols
    Add all symbols to the linker script and make sure they're properly
    exported, by calling them in test-api.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/test-api.c
+++ src/test-api.c
@@ -31,9 +31,86 @@
 static void test_api_constants(void) {
         assert(C_VARIANT_MAX_LEVEL >= (1 << 8) - 1);
         assert(C_VARIANT_MAX_SIGNATURE >= (1 << 16) - 1);
+        assert(C_VARIANT_MAX_VARG >= (1 << 4) - 1);
+}
+
+static void test_api_symbols(void) {
+        struct iovec *vecs;
+        const char *type;
+        CVariant *cv;
+        va_list args;
+        size_t n;
+        int r;
+
+        /* c_variant_new(), c_variant_new_from_vecs(), c_variant_free() */
+
+        r = c_variant_new(&cv, "()", 2);
+        assert(r >= 0);
+
+        cv = c_variant_free(cv);
+        assert(!cv);
+
+        r = c_variant_new_from_vecs(&cv, "()", 2, NULL, 0);
+        assert(r >= 0);
+
+        /* c_variant_{is_sealed,return_poison,get_vecs}() */
+
+        r = c_variant_is_sealed(cv);
+        assert(!!r);
+
+        r = c_variant_return_poison(cv);
+        assert(!r);
+
+        r = c_variant_get_vecs(cv, &vecs, &n);
+        assert(r >= 0);
+
+        /* c_variant_{peek_count,peek_type}() */
+
+        n = c_variant_peek_count(cv);
+
+        type = c_variant_peek_type(cv, &n);
+        assert(!!type);
+
+        /* c_variant_{enter,exit,readv,rewind}() */
+
+        r = c_variant_enter(cv, "(");
+        assert(r >= 0);
+
+        r = c_variant_exit(cv, ")");
+        assert(r >= 0);
+
+        c_variant_rewind(cv);
+
+        r = c_variant_readv(cv, "()", args);
+        assert(r >= 0);
+
+        /* cleanup */
+
+        cv = c_variant_free(cv);
+        assert(!cv);
+
+        /* c_variant_{beginv,end,writev,seal}() */
+
+        r = c_variant_new(&cv, "()", 2);
+        assert(r >= 0);
+
+        r = c_variant_beginv(cv, "(", args);
+        assert(r >= 0);
+
+        r = c_variant_writev(cv, "", args);
+        assert(r >= 0);
+
+        r = c_variant_end(cv, ")");
+        assert(r >= 0);
+
+        /* cleanup */
+
+        cv = c_variant_free(cv);
+        assert(!cv);
 }
 
 int main(int argc, char **argv) {
         test_api_constants();
+        test_api_symbols();
         return 0;
 }
