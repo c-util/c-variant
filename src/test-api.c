848c162cc289026bf38b782e9dc42db510b458bd
Kay Sievers <kay@vrfy.org>
2016-04-25 17:54:58 +0200

    rename cvariant to c-variant, move header file to include root directory

--- src/test-api.c
+++ src/test-api.c
@@ -1,18 +1,18 @@
 /***
-  This file is part of bus1. See COPYING for details.
+  This file is part of c-variant. See COPYING for details.
 
-  bus1 is free software; you can redistribute it and/or modify it
+  c-variant is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation; either version 2.1 of the License, or
   (at your option) any later version.
 
-  bus1 is distributed in the hope that it will be useful, but
+  c-variant is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
   Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public License
-  along with bus1; If not, see <http://www.gnu.org/licenses/>.
+  along with c-variant; If not, see <http://www.gnu.org/licenses/>.
 ***/
 
 /*
@@ -26,7 +26,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "org.bus1/c-variant.h"
+#include "c-variant.h"
 
 static void test_api_constants(void) {
         assert(C_VARIANT_MAX_LEVEL >= (1 << 8) - 1);
